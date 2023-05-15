2c98414c22add1179254b45342b3a682cf4484f5
Vitor Afonso <vittokm@hotmail.com>
2023-05-15 12:23:57 +0100

    reference types

--- src/App.java
+++ src/App.java
@@ -3,7 +3,19 @@ import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("************* REFERENCE TYPES **********");
+
+        Date now = new Date();
+
+        // code snippet for System.out.print() = sout + tab
+        System.out.println(now);
+
+        System.out.println("************* STRINGS **********");
+
+        String message = "Hello world" + "!!";
+
+        System.out.println(message.endsWith("!!"));
 
         
 
