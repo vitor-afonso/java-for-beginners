1443193610716d706e357039c1a7c67f82c30167
Vitor Afonso <vittokm@hotmail.com>
2023-05-15 12:25:59 +0100

    escape sequences

--- src/App.java
+++ src/App.java
@@ -3,9 +3,24 @@ import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
         
+        System.out.println("************* ESCAPE SEQUENCES **********");
+
+        // to add reserved characters to a string like double quotes("") or backslashes(\) we need to escape them with \  
+        String greeting = "Hello \"world\"!";
+        String myPath = "c:windows\\java\\";
+        System.out.println(greeting);
+        System.out.println(myPath);
+
+        // there are more escape sequences like: 
+
+        // \n that breaks to a new line
+        String someName = "Casemiro \n Teixeira";
+        // \t that adds a tab 
+        String pastor = "Pastor\tAndré";
+
+        System.out.println(someName);
+        System.out.println(pastor);
 
     }
 }
