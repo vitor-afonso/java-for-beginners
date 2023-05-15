68aa8ce80d4791ba3ec9a0b6c9eac793373fe264
Vitor Afonso <vittokm@hotmail.com>
2023-05-15 12:18:07 +0100

    Inittial commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        
+
+    }
+}
