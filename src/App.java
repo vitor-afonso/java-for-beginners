102e04229369ac6477586a9506282d2bef02ae4a
Vitor Afonso <vittokm@hotmail.com>
2023-05-16 09:41:56 +0100

    constants

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
-        System.out.println("Hello, World!");
+        // final is the same as const in js
+        // by convention constants are uppercased
+        // the F is to tell java compiler that the number is a float and not a double
+        final float PI = 3.14F;
     }
 }
