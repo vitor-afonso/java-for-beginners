5344a821a9d875eef0152f241ee45dd69d698b5d
Vitor Afonso <vittokm@hotmail.com>
2023-05-20 10:33:40 +0100

    exercise fizzbuzz

--- src/App.java
+++ src/App.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Number:");
+
+        int input = scanner.nextInt();
+
+        int number;
+
+        boolean isDivisibleBy5 = input % 5 == 0;
+        boolean isDivisibleBy3 = input % 3 == 0;
+
+        if (isDivisibleBy3 && isDivisibleBy5) number = 0;
+        else if (isDivisibleBy5) number = 5;
+        else if (isDivisibleBy3 ) number = 3;
+        else number = input;
+
+        switch(number){
+            
+            case 0:
+                System.out.println("FizzBuzz");
+            break;
+            case 5:
+                System.out.println("Fizz");
+            break;
+            case 3:
+                System.out.println("Buzz");
+            break;
+            default:
+                System.out.println(number);
+            
+        }
     }
 }
