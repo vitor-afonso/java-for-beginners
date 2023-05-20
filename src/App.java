688953b757c1f2596ee870c86534e88de71110c5
Vitor Afonso <vittokm@hotmail.com>
2023-05-20 11:07:50 +0100

    do-while-loops

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        // this program will prompt the user to input something at least once and while the value of the input is different than "quit"
+        do {
+            System.out.print("Input: ");
+            input = scanner.nextLine().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
     }
 }
