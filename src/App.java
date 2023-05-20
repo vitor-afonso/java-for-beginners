069694cf8056d652cc3115f8aaac31515a8b6fdf
Vitor Afonso <vittokm@hotmail.com>
2023-05-20 10:58:23 +0100

    while loops

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        // this program will prompt the user to input something until the value of the input equals "quit"
+        while (!input.equals("quit")){
+            System.out.print("Input: ");
+            input = scanner.nextLine().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
