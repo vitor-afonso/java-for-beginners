0402198625eee1bd49c79377f87a89e01015acb3
Vitor Afonso <vittokm@hotmail.com>
2023-05-20 11:27:36 +0100

    for each loop

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
-        System.out.println("Hello, World!");
+        String[] fruits = {"Apple", "Banana", "Mango"};
+        
+        // if we dont need the index use this for each loop
+        // we nhave to specify the type of the items
+        for(String fruit : fruits) 
+            System.out.println(fruit);
     }
 }
